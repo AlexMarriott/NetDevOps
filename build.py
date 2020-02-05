eb36dca0af288025260ac00594f392fe26d277ae
Alex Marriott <alex-marriott@live.co.uk>
2020-02-05 21:02:43 +0000

    Added delete functions, along with listing functions for the droplets.

--- build.py
+++ build.py
@@ -1,25 +1,55 @@
-"""
-This is the main build python file for the network deplyoment
-"""
+"""This is the main build python file for the network deplyoment."""
 
 import os
 import requests
+import json
 
 access_token = ""
 url = "https://api.digitalocean.com/v2/"
 headers = {"Authorization": "Bearer {0}".format(access_token)}
-print(headers)
-data = {
-  "name":[
-    "R1.ain.test"
-  ],
-  "region": "lon1",
-  "size": "s-1vcpu-1gb",
-  "image": "ubuntu-18-04-x64",
-  "backups": False,
-  "ipv6": True,
-  "private_networking": True,
-}
-resp = requests.post("{0}".format(url)  + "droplets", data=data, headers=headers)
-print(resp.text)
-print(resp.status_code)
+
+session = requests.Session()
+session.headers = headers
+
+
+def create_droplet():
+    data = {
+        "names[]": [
+            "R1.ain.test",
+            "R2.ain.test",
+            "SW1.ain.test"
+        ],
+        "region": "lon1",
+        "size": "s-1vcpu-1gb",
+        "image": "ubuntu-18-04-x64",
+        "ssh_keys[]": ["57:c0:a6:98:c8:d1:03:49:7b:2c:af:f0:23:ab:cb:5e"],
+        "backups": "False",
+        "ipv6": "True",
+        "private_networking": "True",
+    }
+    print(data)
+    resp = session.post("{0}".format(url) + "droplets", data=data)
+    print(resp.json())
+    print(resp.status_code)
+
+
+def list_droplets():
+    droplets = []
+    resp = session.get("{0}".format(url) + "droplets", headers=headers)
+    json_resp = json.loads(resp.text)
+    for i in json_resp['droplets']:
+        print(i)
+        droplets.append(i)
+    return droplets
+
+
+def delete_droplet(droplet):
+    resp = session.delete("{0}/droplets/".format(url) + str(droplet))
+    return "{0}".format(resp.status_code)
+
+
+create_droplet()
+
+droplets = list_droplets()
+for i in droplets:
+    print(delete_droplet(i['id']))
