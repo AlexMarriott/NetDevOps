2008666bf8132d2619d8891f9e957956b3350d53
alexmarriott <alex-marriott@live.co.uk>
2020-02-05 15:33:33 +0000

    Early testing of the build.py. Current am able to create servers.

--- build.py
+++ build.py
@@ -1,3 +1,25 @@
 """
 This is the main build python file for the network deplyoment
 """
+
+import os
+import requests
+
+access_token = ""
+url = "https://api.digitalocean.com/v2/"
+headers = {"Authorization": "Bearer {0}".format(access_token)}
+print(headers)
+data = {
+  "name":[
+    "R1.ain.test"
+  ],
+  "region": "lon1",
+  "size": "s-1vcpu-1gb",
+  "image": "ubuntu-18-04-x64",
+  "backups": False,
+  "ipv6": True,
+  "private_networking": True,
+}
+resp = requests.post("{0}".format(url)  + "droplets", data=data, headers=headers)
+print(resp.text)
+print(resp.status_code)
