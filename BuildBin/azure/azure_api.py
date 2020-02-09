60f9dbd74c531ac238a581214e520c7090fb402f
Alex Marriott <alex-marriott@live.co.uk>
2020-02-09 13:31:30 +0000

    Working config, azure is harder to work with.

--- BuildBin/azure/azure_api.py
+++ BuildBin/azure/azure_api.py
@@ -1,6 +1,12 @@
+import json
+
 from .azure_credentials import get_credentials, get_subscription
+from azure.mgmt.resource.resources.models import DeploymentMode
 from azure.mgmt.compute import ComputeManagementClient
 import requests
+import random
+import os
+
 
 class AzureApi:
 
@@ -9,8 +15,56 @@ class AzureApi:
         self.subscription = get_subscription()
         self.resource_group = "TestNetwork"
         self.client = ComputeManagementClient(self.credentials, self.subscription)
+        self.template_path = os.path.abspath("BuildBin/azure/templates/fuckazure.json")
+        with open(self.template_path, 'r') as template_file_fd:
+            self.template = json.load(template_file_fd)
 
+    def create_node(self, vmname="R1TestNode", subnet="Office1"):
 
-    def create_node(self, names=None):
-        create
-        client.virtual_machines.create_or_update(resource_group, )
+            # 'mode': DeploymentMode.incremental,
+            # 'template': self.template,
+            # 'parameters': parameters,
+            # "location": "uksouth"
+        print(self.template)
+        deployment_async_operation = self.client.virtual_machines.create_or_update(self.resource_group, vmname, parameters={
+  "location": "uksouth",
+    "hardwareProfile": {
+      "vmSize": "Standard_B1s"
+    },
+    "storageProfile": {
+      "imageReference": {
+        "sku": "18.04-LTS",
+        "publisher": "Canonical",
+        "version": "latest",
+        "offer": "UbuntuServer"
+      },
+      "osDisk": {
+        "caching": "ReadWrite",
+        "managedDisk": {
+          "storageAccountType": "Standard_LRS"
+        },
+        "name": "myVMosdisk",
+        "createOption": "FromImage"
+      },
+    },
+
+    "osProfile": {
+      "adminUsername": "amarriott",
+      "computerName": "myVM",
+      "adminPassword": "Movingonup2016"
+    },
+    "networkProfile": {
+      "networkInterfaces": [
+        {
+          "id": "/subscriptions/8da87477-14ec-488c-a181-1dbdcc25525e/resourceGroups/TestNetwork/providers/Microsoft.Network/networkInterfaces/memes",
+          "properties": {
+            "primary": "true"
+          }
+        }
+      ]
+    }
+  }
+)
+        deployment_async_operation.wait()
+
+        print(deployment_async_operation)
---
+++ azure_api_test.py
@@ -0,0 +1,34 @@
+import unittest
+from BuildBin.azure.azure_api import AzureApi
+
+
+class AzureApiTest(unittest.TestCase):
+
+    @staticmethod
+    def setup():
+        azure_api = AzureApi()
+        names = ["T1AinTest", "T2AinTest", "T3AinTest"]
+        azure_api.create_droplets(names=names)
+        nodes = azure_api.list_droplets()
+        return nodes
+
+    @staticmethod
+    def clean_up():
+        azure_api = AzureApi()
+        nodes = azure_api.list_droplets()
+        for i in nodes:
+            print("Deleting nodes:{0}".format(i['name']))
+            print("Respones: {0}".format(azure_api.delete_droplet(i['id'])))
+
+    def test_create_node(self):
+        print("Running create azure node test")
+        azure_api = AzureApi()
+        test_nodes = [{"node": "T1AinTest", "nic": 'T1AinTest01'},
+                      {"node": "T2AinTest", "nic": 'T2AinTest02'},
+                      {"node": "T3AinTest", "nic": 'T3AinTest03'}]
+
+        for test_node in test_nodes:
+            resp = azure_api.create_node(vmname=test_node['node'], nic = test_node['nic'])
+            print(resp)
+        #self.assertEqual(resp['status_code'], 202)
+        #self.clean_up()
