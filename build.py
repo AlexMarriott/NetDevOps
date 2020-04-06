744e34b3df9e001033e8a967de1d528dad71cd65
AlexMarriott <alex-marriott@live.co.uk>
2020-04-06 14:33:22 +0100

    renaming the host files.

--- build.py
+++ build.py
@@ -26,7 +26,7 @@ if build_type.upper() == 'LAN':
     id = g.find_project_id()
     print(g.open_project(id))
     print(g.start_nodes(id))
-    ansible = BuildAnsible(build_path("deployment_files", "ansible", "hosts"))
+    ansible = BuildAnsible(build_path("deployment_files", "ansible", "lan_hosts"))
 
     print("Running the deployment scripts")
 
@@ -54,9 +54,9 @@ elif build_type.upper() == 'CLOUD':
     azure_api = AzureApi()
     upload_files = [{"file_name": "install_ansible.sh", "file_path": build_path("deployment_files", "bash")},
                     {"file_name": "deploy_services.yaml", "file_path": build_path("deployment_files", "ansible", "ansible_cloud")},
-                    {"file_name": "all.yaml", "file_path": build_path("deployment_files", "ansible", "ansible_cloud", "group_vars")},
+                    {"file_name": "cloud.yaml", "file_path": build_path("deployment_files", "ansible", "ansible_cloud", "group_vars")},
                     {"file_name": "ansible.cfg", "file_path": build_path("deployment_files", "ansible")},
-                    {"file_name": "hosts", "file_path": build_path("deployment_files", "ansible")},
+                    {"file_name": "cloud_hosts", "file_path": build_path("deployment_files", "ansible")},
                     {"file_name": "service_checker.py", "file_path": build_path("deployment_files", "testcases")},
                     {"file_name": "connectivity_check.py", "file_path": build_path("deployment_files", "testcases")}]
 
@@ -88,13 +88,13 @@ elif build_type.upper() == 'CLOUD':
     print("running deployment files.")
 
     deployment_files = [
-        {"file_name": "all.yaml",
+        {"file_name": "cloud.yaml",
          "url": "https://deploymentscriptsdiss.blob.core.windows.net/deploymentscripts/all.yaml?sp=r&st=2020-04-01T11:37:53Z&se=2031-06-05T19:37:53Z&spr=https&sv=2019-02-02&sr=b&sig=qWIG3oO07wJ3EyjFQqpOAup2R7M0pNxjx7V7vIglTHU%3D"},
         {"file_name": "ansible.cfg",
          "url": "https://deploymentscriptsdiss.blob.core.windows.net/deploymentscripts/ansible.cfg?sp=r&st=2020-04-01T11:40:11Z&se=2030-01-01T20:40:11Z&spr=https&sv=2019-02-02&sr=b&sig=eiVkp9mjnLT43INs3EOS%2BwPQW6awalHp4MkhNrIrndI%3D"},
         {"file_name": "deploy_services.yaml",
          "url": "https://deploymentscriptsdiss.blob.core.windows.net/deploymentscripts/deploy_services.yaml?sp=r&st=2020-04-01T11:44:32Z&se=2030-05-30T19:44:32Z&spr=https&sv=2019-02-02&sr=b&sig=KKerdVfVu9KIW8VMiBXgOHWnV1Q5Kb9w3iymEBEhB2A%3D"},
-        {"file_name": "hosts",
+        {"file_name": "cloud_hosts",
          "url": "https://deploymentscriptsdiss.blob.core.windows.net/deploymentscripts/hosts?sp=r&st=2020-04-01T11:45:11Z&se=2029-09-01T19:45:11Z&spr=https&sv=2019-02-02&sr=b&sig=W5ycHv2doCRT0m7seCU1%2BBmfxvGIue2NIIMdKCWT9rg%3D"},
         {"file_name": "install_ansible.sh",
          "url": "https://deploymentscriptsdiss.blob.core.windows.net/deploymentscripts/install_ansible.sh?sp=r&st=2020-04-01T11:45:35Z&se=2030-08-01T19:45:35Z&spr=https&sv=2019-02-02&sr=b&sig=2RScQbf5W1LlGQOB%2B4lYbS0iFXPriZCpq1VZAdUOSi4%3D"},
@@ -115,15 +115,15 @@ elif build_type.upper() == 'CLOUD':
         print(ssh.exec_command("cd deployment && curl '{0}' > {1} && chmod 777 {1} && dos2unix {1}".format(file["url"],
                                                                                                            file[
                                                                                                                "file_name"])))
-        if file == "all.yaml":
-            ssh.exec_command("mkdir group_vars && mv all.yaml group_vars")
+        if file == "cloud.yaml":
+            ssh.exec_command("mkdir group_vars && mv cloud.yaml group_vars")
 
     print("Running node setup script")
     print(ssh.exec_command("(cd deployment; echo {0} | sudo ./install_ansible.sh)".format(amarriott_password)))
 
     print("Running ansible playbook")
     print(ssh.exec_command(
-        "echo {0} | sudo chmod 777 sshkey.pub sshkey; echo {0} | sudo -s; cd deployment; ansible-playbook -i hosts deploy_services.yaml".format(
+        "echo {0} | sudo chmod 777 sshkey.pub sshkey; echo {0} | sudo -s; cd deployment; ansible-playbook -i cloud_hosts deploy_services.yaml".format(
             amarriott_password)))
 
     print("Running connectivity testing and service testing")
